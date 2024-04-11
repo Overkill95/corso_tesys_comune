d70bd31cb6fc0ece51026fbcf72ba277eba2da18
danielmicheli <danielmicheli@outlook.it>
2024-04-11 16:59:19 +0200

    Aggiunto classi job e department

---
+++ WSEmployeesSoapController/src/main/java/com/controller/dto/JobDto.java
@@ -0,0 +1,46 @@
+package com.controller.dto;
+
+
+
+public class JobDto {
+	
+
+    private Integer jobId;
+	private String jobTitle;
+	private String minSalary;
+	private String maxSalary;
+	  
+	  
+	  public Integer getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Integer jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getMinSalary() {
+		return minSalary;
+	}
+
+	public void setMinSalary(String minSalary) {
+		this.minSalary = minSalary;
+	}
+
+	public String getMaxSalary() {
+		return maxSalary;
+	}
+
+	public void setMaxSalary(String maxSalary) {
+		this.maxSalary = maxSalary;
+	}
+
+}
