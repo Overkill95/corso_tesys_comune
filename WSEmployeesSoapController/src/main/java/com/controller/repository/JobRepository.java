5fbb545efe0fd39ecfd02dab38d3c1481329407b
danielmicheli <danielmicheli@outlook.it>
2024-04-11 16:59:23 +0200

    Aggiunto classi job e department

---
+++ WSEmployeesSoapController/src/main/java/com/controller/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package com.controller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.controller.entity.Job;
+
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Integer>{
+
+}
