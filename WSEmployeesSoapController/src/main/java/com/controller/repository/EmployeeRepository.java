f52d8355f61b459cea70243592e3181a235fa8b9
danielmicheli <danielmicheli@outlook.it>
2024-04-16 16:45:19 +0200

    aggiunto un custom Permission evaluator per dip e employees

---
+++ WSEmployeesSoapController/src/main/java/com/controller/repository/EmployeeRepository.java
@@ -0,0 +1,24 @@
+package com.controller.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.controller.entity.Employee;
+import com.employees.service.EmployeeDto;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	
+	@Query(value = "Select e FROM Employee e INNER JOIN e.user u WHERE u.username = :username")
+	Optional<Employee> getEmplByUsername(@Param("username") String user);
+	
+	List<Employee> findByDepartmentId(Integer departmentId );
+	
+
+}
+
